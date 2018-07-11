ff62e72d943b50e4cedc5fcd6daad5f97d434bb2
Tatiana Voegerl <tatianavoegerl@gmail.com>
2018-07-11 14:22:41 +0000

    working dynamically rendered show.html

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,4 +1,11 @@
 class TodoController < ApplicationController 
     def index
     end
+    def show
+        @todo_title="Read English Intro and Chapter 1"
+        @todo_pomodoro_estimate=5
+        @todo_description="Make sure to use post-it notes!!"
+    end
+    def show2
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   get 'todo/index', to: 'todo#index'
+  get 'todo/show', to: 'todo#show'
+  get 'todo/show2', to: 'todo#show2'
 end
