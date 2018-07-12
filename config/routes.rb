6eae1ccdcc02b91f6426a66a593ae8bb47aaf3a2
Tatiana Voegerl <tatianavoegerl@gmail.com>
2018-07-12 14:10:22 +0000

    fixed some things part 3

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'todo/index', to: 'todo#index'
   get 'todo/new', to: 'todo#new'
   get 'todo/create', to: 'todo#create'
-  get 'todo/edit', to: 'todo#edit'
+  get 'todo/edit/:id', to: 'todo#edit'
   get 'todo/update', to: 'todo#update'
   get 'todo/destroy', to: 'todo#destroy'
   get 'todo/show/:id', to: 'todo#show'
